f26de4b61d8d896ab14e272699bd654c09447493
Andrey <andreydaskal2002@gmail.com>
2024-02-05 01:03:37 +0300

    fix: deleted unnecessary provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
 import { PrismaService } from "src/prisma.service";
 import { UserService } from "src/user/user.service";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
-  providers: [AuthService, UserService, PrismaService, JwtService],
+  providers: [AuthService, UserService, PrismaService],
   controllers: [AuthController],
   exports: [AuthService],
 })
